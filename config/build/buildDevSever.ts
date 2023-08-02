0842cde15f9c7ec7f8d50cd0107c9bc32ee20d3b
Maxim Tikhon <tikhon.maxim@gmail.com>
2023-08-02 11:04:31 +0200

    set up dev server, use env variables

---
+++ config/build/buildDevSever.ts
@@ -0,0 +1,9 @@
+import { BuildOptions } from "./types/config";
+import type { Configuration as DevServerConfiguraton } from 'webpack-dev-server';
+
+export function buildDevServer(options: BuildOptions): DevServerConfiguraton {
+    return {
+        port: options.port,
+        open: true
+    }
+}
