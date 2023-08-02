04bedf02468d1f5076c99c541d8245d4ba8c1766
Maxim Tikhon <tikhon.maxim@gmail.com>
2023-08-02 17:04:12 +0200

    support theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { Link, Route, Routes } from "react-router-dom";
-import './index.scss'
+import './styles/index.scss'
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense } from "react";
+import { Suspense, useContext, useState } from "react";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>Toggle</button>
             <Link to="/">Main</Link>
             <Link to="/about">About</Link>
             <Suspense fallback={<div>Loading... </div>}>
---
+++ src/theme/ThemeProvider.tsx
@@ -0,0 +1,18 @@
+import { FC, useState } from "react";
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext"
+
+const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
+
+const ThemeProvider: FC = ({children}) => {
+    const [theme, setTheme] = useState(defaultTheme );
+
+    return (
+        <ThemeContext.Provider value={{
+            theme,
+            setTheme 
+        }}>{children}
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeProvider;
