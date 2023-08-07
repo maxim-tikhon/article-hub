4ade17b7d271effa45d029e56da91a514564de62
Maxim Tikhon <tikhon.maxim@gmail.com>
2023-08-07 21:16:52 +0200

    add error-boundary library

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
 import React from 'react';
-import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { ErrorBoundary } from 'react-error-boundary';
+import { PageError } from 'widgets/PageError/PageError';
 import App from './app/App';
 import './shared/config/i18n/i18n';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <ErrorBoundary>
+    <ErrorBoundary fallback={<PageError />}>
       <BrowserRouter>
         <ThemeProvider>
           <App />
