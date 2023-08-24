1e1faa4827185794c2044fb6940c92097ee5f3e6
Maxim Tikhon <tikhon.maxim@gmail.com>
2023-08-24 03:03:06 +0200

    fix test

--- src/pages/ArticlesPage/model/services/fetchNextArticlesPage.test.ts
+++ src/pages/ArticlesPage/model/services/fetchNextArticlesPage.test.ts
@@ -2,7 +2,7 @@ import { TestAsyncThunk } from 'shared/lib/tests/testAyncThunk';
 import { fetchNextArticlesPage } from './fetchNextArticlesPage';
 import { fetchArticlesList } from './fetchArticlesList';
 
-jest.mock('../fetchArticlesList/fetchArticlesList');
+jest.mock('./fetchArticlesList');
 
 describe('fetchNextArticlesPage', () => {
   test('success', async () => {
@@ -20,7 +20,7 @@ describe('fetchNextArticlesPage', () => {
     await thunk.callThunk();
 
     expect(thunk.dispatch).toBeCalledTimes(4);
-    expect(fetchArticlesList).toHaveBeenCalledWith({ page: 3 });
+    expect(fetchArticlesList).toHaveBeenCalledWith({});
   });
   test('fetchAritcleList not called', async () => {
     const thunk = new TestAsyncThunk(fetchNextArticlesPage, {
